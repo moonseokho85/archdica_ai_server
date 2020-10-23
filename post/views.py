39b90750e8750088bc27a8091e812cd3165dfa97
Logan Moon <mseokho7733@gmail.com>
2020-10-23 17:51:30 +0900

    add ai model in post folder

--- post/views.py
+++ post/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
+import subprocess
 
 
 class PostView(APIView):
@@ -18,7 +19,13 @@ class PostView(APIView):
     def post(self, request, *args, **kwargs):
         posts_serializer = PostSerializer(data=request.data)
         if posts_serializer.is_valid():
+            print("posts_serializer: ", posts_serializer)
+            print("posts_serializer.data: ", posts_serializer.validated_data)
+            print("posts_serializer.validated_data['type']: ", posts_serializer.validated_data['type'])
+            cmd = 'python3 -u ./post/SSP/test.py --imgs test_data/ --gpus 0 --cfg config/ade20k-hrnetv2.yaml --TEST.result test_result/wall/ --TEST.checkpoint epoch_0.pth --MODEL.object_index 0'
+            subprocess.call(cmd, shell=True)
             posts_serializer.save()
+            print("save successfully!")
             return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
         else:
             print('Error: ', posts_serializer.errors)

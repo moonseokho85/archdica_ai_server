da56e1b3e72dbfb5c9d500b86011340d5e406606
Logan Moon <mseokho7733@gmail.com>
2020-11-02 11:50:00 +0900

    revising admin.py

--- post/admin.py
+++ post/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import Post
+from .models import Conversion, Materials, FindSimilarMaterial
 
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Conversion)
+class ConversionAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'room_image', 'type', 'reference_image', 'wall_mask_image', 'floor_mask_image', 'conversion_image', 'created_at', 'updated_at')
+
+
+@admin.register(Materials)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'sub_brand', 'index', 'type', 'url')
+
+
+@admin.register(FindSimilarMaterial)
+class FindSimilarMaterialAdmin(admin.ModelAdmin):
+    list_display = ('email', 'room_image', 'type')
+
+
 

b17ec8c8faa5054430eb730aff7de520be96f1b7
Logan Moon <mseokho7733@gmail.com>
2020-11-02 15:13:30 +0900

    delete async await

--- post/functions/ssp_execute.py
+++ post/functions/ssp_execute.py
@@ -1,12 +1,12 @@
 import subprocess
 
 
-async def execute(room_image_url):
+def execute(room_image_url):
     wall_cmd = 'python3 -u ./post/SSP/test.py --imgs {0} --gpu 0 --cfg ./post/SSP/config/ade20k-hrnetv2.yaml TEST.result ./post/SSP/test_result/wall/ TEST.checkpoint epoch_0.pth MODEL.object_index 0'.format(
         room_image_url)
     floor_cmd = 'python3 -u ./post/SSP/test.py --imgs {0} --gpu 0 --cfg ./post/SSP/config/ade20k-hrnetv2.yaml TEST.result ./post/SSP/test_result/floor/ TEST.checkpoint epoch_0.pth MODEL.object_index 3'.format(
         room_image_url)
-    await subprocess.call(wall_cmd, shell=True)
-    await subprocess.call(floor_cmd, shell=True)
+    subprocess.call(wall_cmd, shell=True)
+    subprocess.call(floor_cmd, shell=True)
 
     return

a4b6c483ece1705ab5637f7a75c89bc785f59d93
Logan Moon <mseokho7733@gmail.com>
2020-11-03 11:42:19 +0900

    add drf-yasg code in urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -15,9 +15,36 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+from drf_yasg import openapi
 
+schema_url_v1_patterns = [
+    path('api/', include('post.urls', namespace='conversion_api')),
+]
+
+schema_view_conversion = get_schema_view(
+    openapi.Info(
+        title="Jay Open API",
+        default_version='v1',
+        description="안녕하세요. DeepBuild의 API 문서 페이지 입니다.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="mseokho7733@google.com"),
+        license=openapi.License(name="DeepBuild"),
+    ),
+    validators=['flex'], #'ssv'],
+    public=True,
+    permission_classes=(AllowAny,),
+    patterns=schema_url_v1_patterns,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('post.urls'))
+    path('api/', include('post.urls')),
+
+    # Auto DRF API docs
+        url(r'^swagger(?P<format>\.json|\.yaml)/v1$', schema_view_conversion.without_ui(cache_timeout=0), name='schema-json'),
+        url(r'^swagger/v1/$', schema_view_conversion.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        url(r'^redoc/v1/$', schema_view_conversion.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
 ]

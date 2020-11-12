98bae12d243d8defb8f70576e775b02dc2572887
Logan Moon <mseokho7733@gmail.com>
2020-11-12 14:57:36 +0900

    add cors-origin-whitelist

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -113,6 +113,11 @@ DATABASES = {
     }
 }
 
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = {
+    'localhost:3000'
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

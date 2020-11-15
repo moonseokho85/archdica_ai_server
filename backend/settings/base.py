7300afcc322d5f8ce0c91b782b0dfa47119add09
Logan Moon <mseokho7733@gmail.com>
2020-11-15 17:49:32 +0900

    change the method of saving static files on the aws s3

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -198,12 +198,14 @@ else:
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')  # 비밀 액세스 키
 
     AWS_STORAGE_BUCKET_NAME = "archdica-ai-bucket"  # 버킷 이름
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
-        AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
 
+    # Static Setting
     AWS_STATIC_LOCATION = 'static'
-    STATICFILES_STORAGE = 'backend.storages.StaticStorage'
+    STATIC_URL = "https://%s/%s" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
+    # Media Setting
     AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
     DEFAULT_FILE_STORAGE = 'backend.storages.PublicMediaStorage'
 

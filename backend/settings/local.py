e424e87f3ef6eef9bc1670f4ebd70333328cd948
Logan Moon <mseokho7733@gmail.com>
2020-11-19 11:51:26 +0900

    split up the wsgi.py

--- backend/settings/local.py
+++ backend/settings/local.py
@@ -3,3 +3,5 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+WSGI_APPLICATION = 'backend.wsgi.local.application'
--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -3,3 +3,5 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['archdica.ml', 'www.archdica.ml']
+
+WSGI_APPLICATION = 'backend.wsgi.prod.application'

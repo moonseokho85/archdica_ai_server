bc6f494c9c26951503ca262584a7a495b26e4972
Logan Moon <mseokho7733@gmail.com>
2020-12-31 16:27:27 +0900

    reshape of refer

--- post/functions/synthesize.py
+++ post/functions/synthesize.py
@@ -95,6 +95,9 @@ def synthesize(org_image, refer_image, type, scale_factor=None, show_img=False):
         refer = np.asarray(refer_image)
         print("shape of refer: ", refer.shape)
 
+        if len(refer.shape) == 4:
+            refer = refer[:, :, :3]
+
         #     refer_size와 img_size가 동일하거나 refer_size가 작은 경우를 고려해야한다.     #
         print('scale_factor :', scale_factor)
         refer = np.tile(refer, (scale_factor, scale_factor, 1))
